41f054af40e62262e8ab18956861a82716eb4d29
amar434998 <dummymailid990@gmail.com>
2026-08-02 09:32:56 +0530

    master created structure

---
+++ src/test/java/test1/test1.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class test1{
+	
+	public void t1() {
+		System.out.println("amar");
+	}
+	
+	
+}
---
+++ src/test/java/test2/test2.java
@@ -0,0 +1,11 @@
+package test2;
+
+
+public class test2{
+	
+	
+	public void test2() {
+		System.out.println("test2");
+	}
+	
+}
